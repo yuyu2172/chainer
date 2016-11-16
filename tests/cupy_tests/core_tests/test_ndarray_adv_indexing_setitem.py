55b82b3aa6e8ca837c10557484f59b5d1b0cb85d
Yusuke Niitani <yuyuniitani@gmail.com>
2016-11-16 17:31:15 +0900

    WIP add test

---
+++ tests/cupy_tests/core_tests/test_ndarray_adv_indexing_setitem.py
@@ -0,0 +1,23 @@
+import unittest
+
+import itertools
+import numpy
+
+import cupy
+from cupy import testing
+
+
+@testing.parameterize(
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [1, 0], [0, 2]), 'value': 1.},
+    {'shape': (2, 3, 4), 'indexes': (slice(None), [0, 1],  [2, 1]), 'value': 1}
+)
+@testing.gpu
+class TestScatterMultiArrayUpdate(unittest.TestCase):
+
+    @testing.for_all_dtypes()
+    @testing.numpy_cupy_array_equal()
+    def test_scatter_multi_array_update(self, xp, dtype):
+        a = testing.shaped_arange(self.shape, xp, dtype)
+        value = xp.array(self.value, dtype=dtype)
+        a[self.indexes] = self.value
+        return a
